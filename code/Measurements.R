7e6654831c6990d4fc12ea86a28778f2b78a433a
ameliaritger <aritger@ucsb.edu>
2019-10-17 18:26:30 -0700

    incorporate janitor package

--- code/Measurements.R
+++ code/Measurements.R
@@ -1,5 +1,6 @@
 
 library(tidyverse)
+library(janitor)
 
 ## LOAD DATA
 data <- read.csv("data/Measurements.csv")
@@ -7,12 +8,16 @@ data <- read.csv("data/Measurements.csv")
 ## TIDY DATA
 # my own OCD, fix "Trial" issue
 colnames(data)[colnames(data)=="ï..Number"] <- "Number"
+# Clean names for R
+data <- data %>%
+  clean_names()
+# Take the measurements, do algebra
 measure <- data %>%
-  mutate(oral_disk_mm = (Oral_disk_diameter_mm_a + Oral_disk_diameter_mm_b)/2) %>%
-  mutate(wet_mass_g = Wet_mass_vial_g-Vial_mass_g) %>%
-  mutate(dry_mass_g = Dry_mass_vial_g_3-Vial_mass_g) %>%
-  select(ID, oral_disk_mm, Mouth_length_mm, wet_mass_g, dry_mass_g)
+  mutate(oral_disk_mm = (oral_disk_diameter_mm_a + oral_disk_diameter_mm_b)/2) %>%
+  mutate(wet_mass_g = wet_mass_plus_vial_g-vial_mass_g) %>%
+  mutate(dry_mass_g = dry_mass_plus_vial_g_day_3-vial_mass_g) %>%
+  select(id, oral_disk_mm, mouth_length_mm, wet_mass_g, dry_mass_g)
 
 plot(measure$dry_mass_g~measure$oral_disk_mm)
-plot(measure$dry_mass_g~measure$Mouth_length_mm)
-plot(measure$oral_disk_mm~measure$Mouth_length_mm)
+plot(measure$dry_mass_g~measure$mouth_length_mm)
+plot(measure$oral_disk_mm~measure$mouth_length_mm)
