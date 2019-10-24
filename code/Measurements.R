e4975bd8c16e1975b887530b3a54129065040c9f
ameliaritger <aritger@ucsb.edu>
2019-10-24 11:50:38 -0700

    calculate minimum mass measured between day 3 and day 5 when dry mass measured

--- code/Measurements.R
+++ code/Measurements.R
@@ -1,22 +1,31 @@
 
 library(tidyverse)
 library(janitor)
+library(gtools)
 
 ## LOAD DATA
-data <- read.csv("data/Measurements.csv")
+data <- read_csv("data/Measurements.csv")
 
 ## TIDY DATA
 # my own OCD, fix "Trial" issue
 colnames(data)[colnames(data)=="ï..Number"] <- "Number"
-# Clean names for R
+# Remove columns not yet measured
+data <- data[-c(31,34:36),]
+# Clean names for R, convert NAs to 9999 for minimum value calculation next
 data <- data %>%
-  clean_names()
+  clean_names() %>%
+  replace_na(list(dry_mass_plus_vial_g_day_3="9999")) %>%
+  replace_na(list(dry_mass_plus_vial_g_day_4="9999")) %>%
+  replace_na(list(dry_mass_plus_vial_g_day_5="9999"))
 # Take the measurements, do algebra
 measure <- data %>%
   mutate(oral_disk_mm = (oral_disk_diameter_mm_a + oral_disk_diameter_mm_b)/2) %>%
   mutate(wet_mass_g = wet_mass_plus_vial_g-vial_mass_g) %>%
-  mutate(dry_mass_g = dry_mass_plus_vial_g_day_3-vial_mass_g) %>%
-  select(id, oral_disk_mm, mouth_length_mm, wet_mass_g, dry_mass_g)
+  mutate(min_dry_mass_plus_vial_g = pmin(dry_mass_plus_vial_g_day_3,dry_mass_plus_vial_g_day_4,dry_mass_plus_vial_g_day_5)) %>%
+  mutate(dry_mass_g = as.numeric(min_dry_mass_plus_vial_g)-vial_mass_g) %>%
+  select(id, oral_disk_mm, mouth_length_mm,  vial_mass_g, min_dry_mass_plus_vial_g, wet_mass_g, dry_mass_g)
+
+
 
 plot(measure$oral_disk_mm~measure$mouth_length_mm)
 plot(measure$dry_mass_g~measure$oral_disk_mm)
