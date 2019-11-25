f865f150affb8cb3a809e65a99bd0062a58f3387
ameliaritger <aritger@ucsb.edu>
2019-11-25 09:51:33 -0800

    Add 11/18 measurements

--- code/Measurements.R
+++ code/Measurements.R
@@ -14,22 +14,25 @@ data <- data[-c(31,34:36),]
 # Clean names for R, convert NAs to 9999 for minimum value calculation next
 data <- data %>%
   clean_names() %>%
+  replace_na(list(dry_mass_plus_vial_g_day_2="9999")) %>%
   replace_na(list(dry_mass_plus_vial_g_day_3="9999")) %>%
   replace_na(list(dry_mass_plus_vial_g_day_4="9999")) %>%
-  replace_na(list(dry_mass_plus_vial_g_day_5="9999"))
+  replace_na(list(dry_mass_plus_vial_g_day_5="9999")) %>%
+  replace_na(list(dry_mass_plus_vial_g_day_7="9999"))
+
 # Take the measurements, do algebra
 measure <- data %>%
   mutate(oral_disk_mm = (oral_disk_diameter_mm_a + oral_disk_diameter_mm_b)/2) %>%
   mutate(wet_mass_g = wet_mass_plus_vial_g-vial_mass_g) %>%
-  mutate(min_dry_mass_plus_vial_g = pmin(dry_mass_plus_vial_g_day_3,dry_mass_plus_vial_g_day_4,dry_mass_plus_vial_g_day_5)) %>%
+  mutate(min_dry_mass_plus_vial_g = pmin(dry_mass_plus_vial_g_day_2, dry_mass_plus_vial_g_day_3,dry_mass_plus_vial_g_day_4,dry_mass_plus_vial_g_day_5, dry_mass_plus_vial_g_day_7)) %>%
   mutate(dry_mass_g = as.numeric(min_dry_mass_plus_vial_g)-vial_mass_g) %>%
   mutate(basal_disk_mm = (basal_disk_diameter_mm_a + basal_disk_diameter_mm_b)/2) %>%
-  select(id, oral_disk_mm, basal_disk_mm, mouth_length_mm,  vial_mass_g, min_dry_mass_plus_vial_g, wet_mass_g, dry_mass_g)
+  select(id, oral_disk_mm, basal_disk_mm, mouth_length_mm, vial_mass_g, min_dry_mass_plus_vial_g, wet_mass_g, dry_mass_g)
 
 # Make plot of oral disk ~ mouth length, dry mass/wet mass ~ oral disk/mouth length
-ggplot(measure, aes(x=oral_disk_mm, y=wet_mass_g)) +
+ggplot(measure, aes(x=oral_disk_mm, y=dry_mass_g)) +
   geom_point() +
   geom_point(aes(color=id)) + # plot points colored by ID
   geom_smooth(method="lm", se=FALSE, color="grey12", size=1, linetype="dashed") +
-  labs(x="Oral Disk Diameter (mm)", y="Wet mass (g)") +
+  labs(x="Oral Disk Diameter (mm)", y="Dry mass (g)") +
   theme_bw()
