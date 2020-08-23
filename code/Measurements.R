f6860dcb2ca2044d4ce21199f3dd1638ee703199
ameliaritger <47021807+ameliaritger@users.noreply.github.com>
2020-08-23 11:33:38 -0700

    clean up df nomenclature

--- code/Measurements.R
+++ code/Measurements.R
@@ -2,37 +2,55 @@
 library(tidyverse)
 library(janitor)
 library(gtools)
+library(lubridate)
 
 ## LOAD DATA
-data <- read_csv("data/Measurements.csv")
+data <- read_csv("data/Measurements.csv") %>% 
+  clean_names()
 
 ## TIDY DATA
-# my own OCD, fix "Trial" issue
-colnames(data)[colnames(data)=="ï..Number"] <- "Number"
 # Remove columns not yet measured
-data <- data[-c(31,34:36),]
-# Clean names for R, convert NAs to 9999 for minimum value calculation next
-data <- data %>%
-  clean_names() %>%
+#data <- data[-c(31,34:36),]
+# convert NAs to 9999 for minimum value calculation next
+data_9 <- data %>%
   replace_na(list(dry_mass_plus_vial_g_day_2="9999")) %>%
   replace_na(list(dry_mass_plus_vial_g_day_3="9999")) %>%
   replace_na(list(dry_mass_plus_vial_g_day_4="9999")) %>%
   replace_na(list(dry_mass_plus_vial_g_day_5="9999")) %>%
+  replace_na(list(dry_mass_plus_vial_g_day_6="9999")) %>%
   replace_na(list(dry_mass_plus_vial_g_day_7="9999"))
 
 # Take the measurements, do algebra
-measure <- data %>%
+measure <- data_9 %>%
   mutate(oral_disk_mm = (oral_disk_diameter_mm_a + oral_disk_diameter_mm_b)/2) %>%
   mutate(wet_mass_g = wet_mass_plus_vial_g-vial_mass_g) %>%
-  mutate(min_dry_mass_plus_vial_g = pmin(dry_mass_plus_vial_g_day_2, dry_mass_plus_vial_g_day_3,dry_mass_plus_vial_g_day_4,dry_mass_plus_vial_g_day_5, dry_mass_plus_vial_g_day_7)) %>%
+  mutate(min_dry_mass_plus_vial_g = pmin(dry_mass_plus_vial_g_day_2, dry_mass_plus_vial_g_day_3,dry_mass_plus_vial_g_day_4,dry_mass_plus_vial_g_day_5, dry_mass_plus_vial_g_day_6, dry_mass_plus_vial_g_day_7)) %>%
   mutate(dry_mass_g = as.numeric(min_dry_mass_plus_vial_g)-vial_mass_g) %>%
   mutate(basal_disk_mm = (basal_disk_diameter_mm_a + basal_disk_diameter_mm_b)/2) %>%
-  select(id, oral_disk_mm, basal_disk_mm, mouth_length_mm, vial_mass_g, min_dry_mass_plus_vial_g, wet_mass_g, dry_mass_g)
+  select(number, id, oral_disk_mm, basal_disk_mm, mouth_length_mm, vial_mass_g, min_dry_mass_plus_vial_g, wet_mass_g, dry_mass_g) %>% 
+  filter(is.na(dry_mass_g)==FALSE)
+
+mass <- data %>% 
+  mutate("3" = dry_mass_plus_vial_g_day_3,
+         "4" = dry_mass_plus_vial_g_day_4,
+         "5" = dry_mass_plus_vial_g_day_5,
+         "6" = dry_mass_plus_vial_g_day_6,
+         "7" = dry_mass_plus_vial_g_day_7) %>% 
+  pivot_longer("3":"7",
+               names_to = "day",
+               values_to = "dmass") %>% 
+  mutate(year=lubridate::year(as.Date(date, format = "%m/%d/%Y"))) %>% 
+  filter(year=="2020")
+
+#make plot of dry mass measurements over time
+ggplot(mass, aes(x=day, y=dmass, group=number, color=number)) +
+  geom_point() +
+  geom_line()
 
 # Make plot of oral disk ~ mouth length, dry mass/wet mass ~ oral disk/mouth length
 ggplot(measure, aes(x=oral_disk_mm, y=wet_mass_g)) +
   geom_point() +
-  geom_point(aes(color=id)) + # plot points colored by ID
+  #geom_point(aes(color=id)) + # plot points colored by ID
   geom_smooth(method="lm", se=FALSE, color="grey12", size=1, linetype="dashed") +
   labs(x="Oral Disk Diameter (mm)", y="Dry mass (g)") +
   theme_bw()
